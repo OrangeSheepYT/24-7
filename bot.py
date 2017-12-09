46289dd66adda63bb0db2a425836d06f3ab4555f
OrangeSheepYT <mattmarkdonnelly@gmail.com>
2017-12-09 12:41:31 -0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+import asyncio
+import logging
+
+bot = discord.Client()
+bot.logger=logging.getLogger("discord")
+bot.logger.setLevel(logging.DEBUG)
+bot.handler=logging.FileHandler(filename="discord.log",encoding="utf-8",mode='w')
+bot.handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:%(message)s"))
+bot.logger.addHandler(bot.handler)
+
+async def update_config_task():
+    await bot.wait_until_ready()
+    
+    while not bot.is_closed:
+        await asyncio.sleep(60)
+
+@bot.event
+async def on_message(message):
+    pass
+
+@bot.event
+async def on_ready():
+    print("Connected")
+    print("As {}")
+    print("_________")
+
+@bot.event
+async def on_member_join(member):
+    msg = "Hello and welcome {}! Take your time and join our partnering server where you can find more awesome friends and a family friendly community.... New comers will be rewarded with a cookie... uwu :cookie:  Invite: https://discord.gg/wtUMQsB ".format(member.mention)
+    await bot.send_message(member, msg)	
+
+
+bot.run('Mzg5MDgxNzg2MDk5MTcxMzQ4.DQ3b5A.dA-FMDPgSVt2EzQ9EQpQyXeX7Ug')
